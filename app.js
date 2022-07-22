42628a1fd11c71fef1ae909b84ced768dcdd2dbf
Rikayla <Rikaylaj@gmail.com>
2022-07-22 07:16:55 -0400

    inquirer working

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
+
+//console.log(inquirer);
+
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template');
+
+
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out idex.html to see the output');
+
+// });
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+
+    .then(answers => console.log(answers));
